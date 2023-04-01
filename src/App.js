efb409c6c7098cf8776bd83be1597e5459f2c706
adamanandas <adamananda02@gmail.com>
2023-04-01 21:25:35 +0700

    add fucntionality for new expense form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummyExpenses = [
+  {
+    id: "1",
+    title: "Pajak Penghasilan",
+    amount: 35_000_000,
+    date: new Date(2023, 1, 10),
+  },
+  {
+    id: "2",
+    title: "Pajak Bumi dan Bangunan",
+    amount: 2_500_00,
+    date: new Date(2023, 1, 15),
+  },
+  {
+    id: "3",
+    title: "Pajak Kendaraan",
+    amount: 15_000_000,
+    date: new Date(2023, 1, 20),
+  },
+  {
+    id: "4",
+    title: "Gaji",
+    amount: 250_000_000,
+    date: new Date(2023, 1, 25),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "1",
-      title: "Pajak Penghasilan",
-      amount: 35_000_000,
-      date: new Date(2023, 1, 10),
-    },
-    {
-      id: "2",
-      title: "Pajak Bumi dan Bangunan",
-      amount: 2_500_00,
-      date: new Date(2023, 1, 15),
-    },
-    {
-      id: "3",
-      title: "Pajak Kendaraan",
-      amount: 15_000_000,
-      date: new Date(2023, 1, 20),
-    },
-    {
-      id: "4",
-      title: "Gaji",
-      amount: 250_000_000,
-      date: new Date(2023, 1, 25),
-    },
-  ];
+  const [expenses, setExpenses] = useState(dummyExpenses);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
 
   return (
